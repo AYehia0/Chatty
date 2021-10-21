27f6b4fb7c1beba81a9ea0fd6cba97c4691cd105
AYehia0 <ahmedyehia611@gmail.com>
2021-10-21 23:47:47 +0200

    fixed socketIO not working and added on connect/disconnect

--- server.js
+++ server.js
@@ -1,22 +1,41 @@
 const path = require("path")
 const express = require("express")
-const server = require("http")
-const socketIO = require("socket.io")
-
 const app = express()
-const io = socketIO()
+
+// Messages
+const welcomeMsg = "Welcome to the chatapp"
+const connectMsg = "User has been connected"
+const disconnectMsg = "User has been disconnected"
+
+// creating the server
+const server = require("http").Server(app)
+const io = require("socket.io")(server)
 
 const PORT = process.env.PORT || 8080
 
 // listening the user's connections
 io.on("connection", (socket) => {
-  console.log("Connection has been created")
+  // welcome the user
+  socket.emit("message", welcomeMsg)
+
+  // brodcasting to all the users except the the one who joined
+  socket.broadcast.emit("message", connectMsg)
+
+  // on disconnect
+  socket.on("disconnect", () => {
+    io.emit("message", disconnectMsg)
+  })
+
+  // checking for chat messages
+  socket.on("chatMessages", (message) => {
+    io.emit("message", message)
+  })
 })
 
 // serving the the static files : default is public
 app.use(express.static(path.join(__dirname, "public")))
 
 // Server is running
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Listening on port : ${PORT}`)
 })

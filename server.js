0261473a123ca0ba4ca020c83ff3c3aed68699f7
AYehia0 <ahmedyehia611@gmail.com>
2021-10-23 23:06:58 +0200

    listening for roomjoining and disconnection

--- server.js
+++ server.js
@@ -2,10 +2,22 @@ const path = require("path")
 const express = require("express")
 const app = express()
 
+// Main bot
+const bot = "BOT"
+
 // Messages
+const getMessageFormatted = require("./utils/messages")
 const welcomeMsg = "Welcome to the chatapp"
-const connectMsg = "User has been connected"
-const disconnectMsg = "User has been disconnected"
+const connectMsg = "has been connected"
+const disconnectMsg = "has been disconnected"
+
+// Users
+const {
+  getAllUsers,
+  getUserById,
+  disconnectUser,
+  joinUser,
+} = require("./utils/users")
 
 // creating the server
 const server = require("http").Server(app)
@@ -13,22 +25,41 @@ const io = require("socket.io")(server)
 
 const PORT = process.env.PORT || 8080
 
+
 // listening the user's connections
 io.on("connection", (socket) => {
-  // welcome the user
-  socket.emit("message", welcomeMsg)
 
-  // brodcasting to all the users except the the one who joined
-  socket.broadcast.emit("message", connectMsg)
+  // room joining
+  socket.on("roomJoin", ({ username, room }) => {
 
-  // on disconnect
-  socket.on("disconnect", () => {
-    io.emit("message", disconnectMsg)
+
+    console.log(`${username} has joined ${room}`)
+    const user = joinUser(socket.id, username, room)
+
+    socket.join(user.room)
+
+    // welcome the user
+    socket.emit("message", getMessageFormatted(bot, welcomeMsg))
+
+    // brodcasting to all the users except the the one who joined
+    socket.broadcast
+      .to(user.room)
+      .emit("message", getMessageFormatted(bot, `${username} ${connectMsg}`))
   })
 
   // checking for chat messages
   socket.on("chatMessages", (message) => {
-    io.emit("message", message)
+
+    const currentUser = getUserById(socket.id)
+
+    io.to(currentUser.room).emit("message", getMessageFormatted(currentUser.username, message) )
+  })
+
+  // on disconnect
+  socket.on("disconnect", () => {
+
+    const user = disconnectUser(socket.id)
+    io.to(user.room).emit("message", getMessageFormatted(bot, `${user.username} ${disconnectMsg}` ))
   })
 })
 

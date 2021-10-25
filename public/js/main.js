5e4637c51902796a8d93aa80f806732581e5f60e
AYehia0 <ahmedyehia611@gmail.com>
2021-10-25 21:19:30 +0200

    changing the html with room details

--- public/js/main.js
+++ public/js/main.js
@@ -9,6 +9,8 @@ const { username, room } = Qs.parse(location.search, {
 // Document
 const messageForm = document.getElementById("chat-form")
 const chatMessagesDiv = document.querySelector(".chat-messages")
+const roomNameEl = document.querySelector('#room-name')
+const roomMembersUl = document.querySelector('#users')
 
 // Main functions
 const addMessageToHtml = (message) => {
@@ -27,9 +29,40 @@ const addMessageToHtml = (message) => {
   chatMessagesDiv.appendChild(messageEl)
 }
 
+// room html details 
+
+const showRoomDetails = (roomName, usersInRoom) => {
+
+  // setting the name for the room 
+  roomNameEl.innerHTML = roomName
+
+
+  // clearing the members
+  roomMembersUl.innerHTML = ""
+
+  // adding the members
+  usersInRoom.forEach(user => {
+
+    const userLi = document.createElement("li")
+
+    userLi.innerHTML = `<li>${user.username}</li>`
+
+    roomMembersUl.appendChild(userLi)
+  })
+
+}
+
 // joining a room
 // catch it in the server side
 socket.emit("roomJoin", { username, room })
+
+// listing all the users
+socket.on('roomUsers', ({room , users}) => {
+
+  // show users to the html 
+  showRoomDetails(room, users)
+
+})
 
 socket.on("message", (message) => {
   // add the messages to the html
